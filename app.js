dd047e8b18553ac0256e1529951fdea674ef4ab6
MBouinBio <memoiredi@gmail.com>
2026-09-12 16:18:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 const CONFIG = {
     // Remplacer par l'URL de l'exécutable Web de ton Google Apps Script
     GAS_URL: "https://script.google.com/macros/s/AKfycbxc-27rxdY0uz0og3X8HrF70u4xup_9mSjTDmtniiIcx2nDp1tdOShVVqBgcKtdRWY/exec",
-    TOKEN: "REMPLACER_PAR_TON_TOKEN", // Doit correspondre au token du Code.gs
+    TOKEN: "123veille456STS", // Doit correspondre au token du Code.gs
     ITEMS_PER_PAGE: 12
 };
 
